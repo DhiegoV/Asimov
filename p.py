6f1698e148b2ccaa6eea78de497140e86dcc7d07
DhiegoV <dhiegovinicius1@gmail.com>
2018-04-19 18:18:45 -0300

    atualizando p.py com o que tava no robô

--- p.py
+++ p.py
@@ -3,20 +3,16 @@
 from ev3dev.ev3 import *
 from time import sleep
 
-#dir preto +- 5
-#esq preto +- 4
-#esq branco +- 55
-#dir branco +- 54
-
-KP = 10
-TP= 100
+KP = 5.5
+TP= 120
 OFFSET = 0
 
 #MOTORES
 esq = LargeMotor('outA')
 dir = LargeMotor('outB')
-motor_garra = LargeMotor('outC')
-motor_sensor = MediumMotor('outD')
+#motor_garra = LargeMotor('outC')
+#motor_sensor = MediumMotor('outD')
+
 
 #SENSORES
 sensor_esq = ColorSensor(address=INPUT_1)
@@ -27,26 +23,36 @@ sensor_esq.mode = 'COL-REFLECT'
 sensor_dir.mode = 'COL-REFLECT'
 
 def sat(giro):
+    POT_MAX = 1000
+    POT_MIN = - 1000
     '''
     Satura o valor do giro para não ultrapassar o valor máximo do motor
-    :param giro: KP*erro +ou- TP
+    :para giro: KP*erro +ou- TP
     :return: O valor do giro saturado
     '''
-    if giro > 1000:
-        return 1000
-    if giro < -1000:
-        return -1000
+    if giro > POT_MAX:
+        return POT_MAX
+
+    if giro < POT_MIN:
+        return POT_MIN
+
     return giro
 
-def executar():
-        while True:
-                erro = (sensor_dir.value() - sensor_esq.value())
-                p=KP*erro
-                print(p)
-                giro_dir = sat(TP+p)
-                giro_esq = sat(TP-p)
-                print(giro_esq)
-                esq.run_forever(speed_sp=giro_esq)
-                dir.run_forever(speed_sp=giro_dir)
-executar()
 
+def ver_verde(giro_esq, giro_dir):   # FUNÇÃO INCOMPLETA
+    # sensor_esq.mode="RGB-RAW" -->> PENSANDO EM UTILIZAR COM MÓDULO RGB.
+    # sensor_dir.mode="RGB-RAW"
+
+
+def executar():
+    while True:
+        erro = (sensor_dir.value() - sensor_esq.value())
+        p = KP * erro
+        giro_dir = sat(TP+p)
+        giro_esq = sat(TP-p)
+        # print(sensor_esq.value(), giro_esq, giro_dir, sensor_dir.val ue(), p)
+        esq.run_forever(speed_sp=giro_esq)
+        dir.run_forever(speed_sp=giro_dir)
+        #ver_verde(giro_esq,giro_dir)
+
+executar()
