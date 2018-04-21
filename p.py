23a9c2ebeff5738dbe7ba3555f807ce5a3cf27af
DhiegoV <dhiegovinicius1@gmail.com>
2018-04-21 15:17:09 -0300

    limpando p.py de código inútil ou meio feio

--- p.py
+++ p.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 
 from ev3dev.ev3 import *
-from time import sleep
 from portas_modos import *
 
 KP = 5.5
-TP= 120
+TP = 120
 OFFSET = 0
 
 def sat(giro):
-    POT_MAX = 1000
-    POT_MIN = - 1000
     '''
     Satura o valor do giro para não ultrapassar o valor máximo do motor
     :para giro: KP*erro +ou- TP
     :return: O valor do giro saturado
     '''
+
+    POT_MAX = 1000
+    POT_MIN = -1000
+
     if giro > POT_MAX:
         return POT_MAX
 
@@ -24,23 +25,14 @@ def sat(giro):
 
     return giro
 
-
-def ver_verde(giro_esq, giro_dir):   # FUNÇÃO INCOMPLETA
-	pass
-    # sensor_esq.mode="RGB-RAW" -->> PENSANDO EM UTILIZAR COM MÓDULO RGB.
-    # sensor_dir.mode="RGB-RAW"
-
-
 def executar():
     while True:
         erro = (sensor_dir.value() - sensor_esq.value())
         p = KP * erro
-        giro_dir = sat(TP+p)
-        giro_esq = sat(TP-p)
-        # print(sensor_esq.value(), giro_esq, giro_dir, sensor_dir.val ue(), p)
+        giro_dir = sat(TP + p)
+        giro_esq = sat(TP - p)
         esq.run_forever(speed_sp=giro_esq)
         dir.run_forever(speed_sp=giro_dir)
-        #ver_verde(giro_esq,giro_dir)
 
 executar()
 
