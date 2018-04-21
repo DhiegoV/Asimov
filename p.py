ea1abdf3292b346866bc13d36963899da1b5498b
DhiegoV <dhiegovinicius1@gmail.com>
2018-04-21 15:25:00 -0300

    adicionando código que reconheça os valores calibrados, falta testar

--- p.py
+++ p.py
@@ -2,6 +2,15 @@
 
 from ev3dev.ev3 import *
 from portas_modos import *
+from json import load
+
+arq_dir = open(r, "sensor_direita.json")
+direita = load(arq_dir)
+arq_dir.close()
+
+arq_esq = open(r, "sensor_esquerda.json")
+esquerda = load(arq_esq)
+arq_esq.close()
 
 KP = 5.5
 TP = 120
@@ -25,9 +34,29 @@ def sat(giro):
 
     return giro
 
+def get_valor_sensor_direita():
+    # Retorna o valor do sensor da direita calibrado, na escala de 0-1000
+
+    valor = (
+        (direita["branco"] - sensor_dir.value()) /
+        (direita["branco"] - direita["preto"])
+    ) * -1000 + 1000
+
+    return valor
+
+def get_valor_sensor_esquerda():
+    # Retorna o valor do sensor da esquerda calibrado, na escala de 0-1000
+
+    valor = (
+        (esquerda["branco"] - sensor_esq.value()) /
+        (esquerda["branco"] - esquerda["preto"])
+    ) * -1000 + 1000
+
+    return valor
+
 def executar():
     while True:
-        erro = (sensor_dir.value() - sensor_esq.value())
+        erro = get_valor_sensor_direita() - get_valor_sensor_esquerda()
         p = KP * erro
         giro_dir = sat(TP + p)
         giro_esq = sat(TP - p)
