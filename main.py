eea29feddccbd22e23c2acecf6466921bb947e89
DhiegoV <dhiegovinicius1@gmail.com>
2018-05-18 20:44:14 +0000

    robo ja faz o verde, mas falta andar um pouco para nao ve-lo de novo

--- main.py
+++ main.py
@@ -18,35 +18,81 @@ KI = 0
 KD = 0
 TP = 120
 
+def compensar_verde(momento):
+	# Anda um pouco para frente para evitar que o robo veja o verde novamente
+	# apos o giro de 90
+
+	# ate_eixo_rotacao eh a distancia em graus para o motor girar ate que o eixo de
+	# rotacao do robo seja alcancado
+
+	# TO DO!
+	if momento == 'antes':
+		ate_eixo_rotacao = 160
+		v_ajuste = 80
+
+	Sound.beep()
+	dir.run_to_rel_pos(position_sp=-ate_eixo_rotacao, speed_sp=v_ajuste)
+	esq.run_to_rel_pos(position_sp=-ate_eixo_rotacao, speed_sp=v_ajuste)
+	esq.wait_while('running')
+
+
 def girar(sentido):
-    # Gira o robô no próprio eixo 90 graus no dado sentido
+	# Gira o robô no próprio eixo 90 graus no dado sentido
 
-    # direita é um valor .....
-    # esquerda é um valor .....
-    velocidade = 100
-    noventa_graus_em_rotacoes = 90
+	velocidade = 400
 
-    if sentido == 'esquerda':
-        dir.on_for_rotations(velocidade, noventa_graus_em_rotacoes)
-        esq.on_for_rotations(-velocidade, noventa_graus_em_rotacoes)
-    elif sentido == 'direita':
-        dir.on_for_rotations(-velocidade, noventa_graus_em_rotacoes)
-        esq.on_for_rotations(velocidade, noventa_graus_em_rotacoes)
-    else:
-        print('SENTIDO INFORMADO ERRONEAMENTE')
+	# direita é um vaor positivo e esquerda é um valr negativo, pois positivo
+	# eh sentido horario
+	# 90 graus p/ robo = 420 graus p/ motor
+	quanto_rodar = 420
+
+	if sentido == 'esquerda':
+		compensar_verde()
+
+		Sound.beep()
+		dir.run_to_rel_pos(position_sp=-quanto_rodar, speed_sp=velocidade)
+		esq.run_to_rel_pos(position_sp=quanto_rodar, speed_sp=velocidade)
+		esq.wait_while('running')
+
+		compensar_verde()
+	elif sentido == 'direita':
+		compensar_verde()
+
+		Sound.beep()
+		dir.run_to_rel_pos(position_sp=quanto_rodar, speed_sp=velocidade)
+		esq.run_to_rel_pos(position_sp=-quanto_rodar, speed_sp=velocidade)
+		esq.wait_while('running')
+
+		compensar_verde()
+	else:
+		print('SENTIDO INFORMADO ERRONEAMENTE')
 
 def rotina_verde():
-    # Verifica se algum dos sensores vê verde e gira de acordo
+	# Verifica se algum dos sensores vê verde e gira de acordo
 
-    modo_anterior = sensor_esq.mode
-    sensor_esq.mode = 'COL-COLOR'
-    sensor_dir.mode = 'COL-COLOR'
+	modo_anterior = sensor_esq.mode
+	sensor_esq.mode = 'COL-COLOR'
+	sensor_dir.mode = 'COL-COLOR'
 
-    # se o sensor vê verde, cor 3
-    if sensor_esq.value() == 3:
-        girar('esquerda', 90)
-    elif sensor_dir.value() == 3:
-        girar('direita', 90)
+	# se o sensor vê verde, cor 3
+	if sensor_esq.value() == 3:
+		girar('esquerda')
+	elif sensor_dir.value() == 3:
+		girar('direita')
+
+	sensor_esq.mode = modo_anterior
+	sensor_dir.mode = modo_anterior
+
+def parece_verde():
+	# Verifica se o valor de reflectancia atual parece com o de
+	# reflectancia de verde, retornando True se sim, e False se não.
+
+	if sensor_dir.value() in range(direita["verde_min"], direita["verde_max"]):
+		return True
+	elif sensor_esq.value() in range(esquerda["verde_min"], esquerda["verde_max"]):
+		return True
+	else:
+		return False
 
 def sat(giro):
 	'''
@@ -92,6 +138,9 @@ def executar():
 	botao = Button()
 
 	while not botao.any():
+		if parece_verde():
+			rotina_verde()
+
 		erro = get_valor_sensor_direita() - get_valor_sensor_esquerda()
 		pid.update(erro)
 		correcao = pid.output
